d7bd29cd2ab5794529a7ccd7134d5ab62c6842af
ironprayer <ironprayer@naver.com>
2021-08-07 21:30:49 +0900

    코드 수정

--- src/App.tsx
+++ src/App.tsx
@@ -6,37 +6,25 @@ import { render } from '@testing-library/react';
 
 class App extends React.Component {
   state = {
-    iscoffee: true,
-    isfood: true
+    answerIndex:0,
+    totalScore:0
   };
 
-  getState() {
-    if(this.state.iscoffee == true){
-      this.setState({iscoffee:false});
-    }else{
-      this.setState({iscoffee:true});
-    }
+  changeTotalScore(score:number) {
+    this.setState({totalScore:this.state.totalScore + score})
+  }
+
+  changeIndex(){
+    this.setState({answerIndex:this.state.totalScore + 1})
   }
 
   render(){
-    const {iscoffee, isfood} = this.state;
-    const qnas = QuestionAnswer.getQuestion()
+    const totalScore:number = this.state.totalScore;
+    const answerIndex:number = this.state.answerIndex;
     return (
       <div>
-        {iscoffee?(
-        <QuestionAnswer
-          question={qnas[0].question}
-          answer_one={qnas[0].answer_one}
-          answer_two={qnas[0].answer_two} 
-        />) : (
-          <QuestionAnswer
-          question={qnas[1].question}
-          answer_one={qnas[1].answer_one}
-          answer_two={qnas[1].answer_two}
-        />          
-        )
-      }
-      <button onClick={this.getState.bind(this)}></button>
+        <QuestionAnswer/>  
+        <button onClick={this.changeIndex.bind(this)}></button>
       </div>
     )
   }
--- src/components/questionAnswer.tsx
+++ src/components/questionAnswer.tsx
@@ -8,10 +8,12 @@ type qnaInfo = {
     answer_two_score : number;
 }
 
-class QuestionAnswer extends React.Component<{question:string; answer_one:string; answer_two:string}>{
-
-    static getQuestion():qnaInfo[]{
-        return [
+class QuestionAnswer extends React.Component{
+    state = {
+        answerIndex:0,
+        totalScore:0
+    }
+    _qnas:qnaInfo[] = [
             {
                 question:"오늘 밥은 뭐야?",
                 answer_one:"치킨",
@@ -27,25 +29,37 @@ class QuestionAnswer extends React.Component<{question:string; answer_one:string
                 answer_two_score:2
             },
             {
-                question:"오늘 밥은 뭐야?",
-                answer_one:"치킨",
+                question:"도레미",
+                answer_one:"도도",
                 answer_one_score:1,
-                answer_two:"피자",
+                answer_two:"도레미",
                 answer_two_score:2
             }
         ]
+    
+    processForNextStep(selectNum:number = 1 | 2){
+        const answerIndex:number = this.state.answerIndex;
+        if(selectNum == 1){
+            console.log(1);
+        }
+        this.increaseAnswerIndex()
+    }
+
+    increaseAnswerIndex(){
+        console.log(this.setState({answerIndex:this.state.answerIndex + 1}))
+        console.log(this.state)
     }
 
     render(){
-        const{question, answer_one, answer_two} = this.props;
+        const answerIndex:number = this.state.answerIndex;
         return(
             <div className="ttt">
                 <h1>질문</h1>
-                <div>{question}</div>
+                <div>{this._qnas[answerIndex%3].question}</div>
                 <h1>대답</h1>
-                <button>{answer_one}</button>
+                <button onClick={this.processForNextStep.bind(this,1)} >{this._qnas[answerIndex%3].answer_one}</button>
                 <h1>대답2</h1>
-                <div>{answer_two}</div>
+                <button onClick={this.processForNextStep.bind(this,2)}>{this._qnas[answerIndex%3].answer_two}</button>
             </div>
         )
     }
