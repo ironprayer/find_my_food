3d5b884dc94c43e4d50ff26086094f8c0fa417de
ironprayer <ironprayer@naver.com>
2021-08-09 21:31:52 +0900

    start 수정

--- src/components/start.tsx
+++ src/components/start.tsx
@@ -1,5 +1,17 @@
 import * as React from "react";
 
 interface Start{
+    nextPage():void;
+}
+
+class StartPage{
+    nextPage(){
+
+    }
     
+    render(){
+        <div>
+            <button onClick={this.nextPage}>시작하기</button>
+        </div>
+    }
 }
